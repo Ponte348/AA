6950c292bee1b2c80fdddd8012cd41960f3115c0
Ponte348 <ponte@ua.pt>
2024-10-08 16:01:56 +0100

    2D array bem feito ig

--- Aulas/Aula04/delannoy.py
+++ Aulas/Aula04/delannoy.py
@@ -11,7 +11,15 @@ def twoD_array_delannoy(m, n):
     if m == 0 or n == 0:
         return 1
     matrix = [[0 for i in range(n+1)] for j in range(m+1)]
-
+    for i in range(m+1):
+        matrix[i][0] = 1
+    for j in range(n+1):
+        matrix[0][j] = 1
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            matrix[i][j] = matrix[i-1][j] + matrix[i][j-1] + matrix[i-1][j-1]
+    return matrix[m][n]
+    
 def main(n, method):
     if method == "recursion":
         recursion_taken = []
@@ -57,6 +65,27 @@ def main(n, method):
         plt.savefig('2D_array_delannoy_plot.png')
         print("Plot saved as '2D_array_delannoy_plot.png'")
 
+    elif method == "two 1D arrays":
+        two_1D_arrays_taken = []
+        for i in range(n+1):
+            start = perf_counter_ns()
+            print("Delannoy(",i,",",i,"):", two_1D_arrays_delannoy(i, i), 
+                "; Time taken:", round((perf_counter_ns() - start)/1000000000, 2), "s")
+            taken = perf_counter_ns() - start
+            two_1D_arrays_taken.append(taken)
+            if (taken > 1000000000):
+                print("It's taking too much time!")
+                break
+        
+        plt.plot(range(len(two_1D_arrays_taken)), two_1D_arrays_taken, marker='o', label='Two 1D Arrays Time')
+        plt.xlabel('n')
+        plt.ylabel('Time (seconds)')
+        plt.title('Two 1D Arrays Time for Delannoy Numbers')
+        plt.legend()
+        plt.grid(True)
+        # Save the plot to a file
+        plt.savefig('two_1D_arrays_delannoy_plot.png')
+        print("Plot saved as 'two_1D_arrays_delannoy_plot.png'")
 if __name__ == "__main__":
     # ask user for input
     method = input("Enter the method you want to use (recursion, 2D array, two 1D arays, memoization): ")
