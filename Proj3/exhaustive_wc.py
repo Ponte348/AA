2986a6b45582a27671bbe42afd81875b16ba0c18
Ponte348 <ponte@ua.pt>
2024-12-26 19:50:16 +0000

    output dir configurable

--- Proj3/exhaustive_wc.py
+++ Proj3/exhaustive_wc.py
@@ -1,6 +1,6 @@
 from file_processing import getProcessedFiles
 
-def countWordOccurrences(file):
+def countWordOccurrences(file, stats_dir="exhaustive_wc_stats"):
     print(f"Counting word occurrences in {file}")
     
     with open(file, 'r') as f:
@@ -24,7 +24,7 @@ def countWordOccurrences(file):
     sorted_words = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
     
     # save stats
-    stats_file = "exhaustive_wc_stats/" + file.split("/")[-1]
+    stats_file = stats_dir + file.split("/")[-1]
     with open(stats_file, 'w') as f:
         f.write(f"Number of lines (excluding empty): {num_lines}\n")
         f.write(f"Number of words: {num_words}\n")
